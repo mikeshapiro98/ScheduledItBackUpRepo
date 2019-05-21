644935327edc2166962dbbb865f5ce0ba2671c90
Mike Shapiro <michaelshapiro98@gmail.com>
2019-05-21 12:55:32 -0700

    Add updated EventModel for back up

---
+++ ScheduleIt2.0/Models/EventModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScheduleIt2._0.Models
+{
+    public class EventModel
+    {
+        /// <summary>
+        ///event id property
+        /// </summary>
+        public Guid EventId { get; set; }
+
+        /// <summary>
+        ///start time of event property
+        /// </summary>
+        public DateTime StartTime { get; set; }
+
+        /// <summary>
+        ///end time of event property
+        /// </summary>
+        public DateTime EndTime { get; set; }
+
+        /// <summary>
+        ///note attached to event property
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        ///title of event property
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        ///id of admin who approved event property
+        /// <summary>
+        public int AdminId { get; set; }
+
+        /// <summary>
+        ///conntects to attached user
+        /// </summary>
+        public virtual ApplicationUser User { get; set; }
+    }
+}
+
+    
+    
