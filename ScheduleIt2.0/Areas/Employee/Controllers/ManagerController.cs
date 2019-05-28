d958306562a7d8144e84185973c8613f1424803b
Mike Shapiro <michaelshapiro98@gmail.com>
2019-05-28 08:55:13 -0700

    update project

---
+++ ScheduleIt2.0/Areas/Employee/Controllers/ManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScheduleIt2._0.Areas.Employee.Controllers
+{
+    public class ManagerController : Controller
+    {
+        // GET: Employee/Manager
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
